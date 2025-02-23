b4ffb2213fbf52a96aff0af994ad71d1d67626ab
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-24 03:28:28 +0600

    Refactor user model to improve database queries and enhance readability

--- src/modules/auth/models/auth.model.js
+++ src/modules/auth/models/auth.model.js
@@ -1,18 +1,26 @@
 const { db } = require('./../../../config/firebase.config');
 
+const usersCollection = db.collection('db_users');
+
 
 // create user
-exports.createUser = async(userId, data)=>{
-    try{
-        await db.collection('db_users').doc(userId).set(data);
-    }catch(error){
+exports.createUser = async (userId, data) => {
+    try {
+        await usersCollection.doc(userId).set(data);
+    } catch (error) {
         console.log('Error creating user: ', error);
     }
 };
 
 
 // get user by email
-exports.getUserByEmail = async(email)=>{
-    const snapshot = await db.collection('db_users').where('email', '==', email).get(); 
-    return snapshot.empty? null: snapshot.docs[0].data();   
+exports.getUserByEmail = async (email) => {
+    const querySnapshot = await usersCollection.where('email', '==', email).limit(1).get();
+
+    if (querySnapshot.empty) {
+        return null;
+    }
+
+    const userDoc = querySnapshot.docs[0];
+    return { userId: userDoc.id, ...userDoc.data() };
 };
