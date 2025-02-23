24e0dd99dc4a9f18a860e8c976cc5e357660d130
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-23 18:42:32 +0600

    Add authentication route to main application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const testRoute = require('./modules/test/routes/test.route');
+const authRoute = require('./modules/auth/routes/auth.route');  
+
 require('dotenv').config();
 
 
@@ -8,6 +10,7 @@ app.use(express.json());
 
 
 app.use('/', testRoute);
+app.use('/api/auth',authRoute);
 
 
 const PORT = process.env.PORT || 3000;
