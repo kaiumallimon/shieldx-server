1d72443a631593b0770d1b158c0fa109da0c7223
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-25 21:48:15 +0600

    Add password management routes to the main application

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const testRoute = require('./modules/test/routes/test.route');
 const authRoute = require('./modules/auth/routes/auth.route');  
 const userProfileRoute = require('./modules/user/routes/user.routes');
+const passwordRoute = require('./modules/passwords/routes/passwords.route');
 require('dotenv').config();
 
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/', testRoute);
 app.use('/api/auth',authRoute);
 app.use('/api/profile',userProfileRoute);
+app.use('/api/password',passwordRoute);
 
 
 const PORT = process.env.PORT || 3000;
